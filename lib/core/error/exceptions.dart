cb7aa9498715dedebc304a7742e08f1a38a7da64
KSTA <>
2025-07-04 16:35:08 +0630

    Add error handling classes for common exceptions, and implement failure interpretation logic for better error management.

---
+++ lib/core/error/exceptions.dart
@@ -0,0 +1,16 @@
+class NetworkException implements Exception {}
+
+class ParseException implements Exception {}
+
+class UnauthorizedException implements Exception {}
+
+class ForbiddenException implements Exception {}
+
+class NotFoundException implements Exception {}
+
+class BadRequestException implements Exception {}
+
+class InternalServerErrorException implements Exception {}
+
+class ServiceUnavailableException implements Exception {}
+
---
+++ lib/core/error/failures.dart
@@ -0,0 +1,77 @@
+import 'package:flutter_graphql_subscription_test/core/error/exceptions.dart';
+
+class Failure {
+  final Exception exception;
+  final StackTrace? stackTrace;
+
+  final FailureInterpretation interpretation;
+
+  Failure({required this.exception, this.stackTrace})
+      : interpretation =
+            FailuerInterpretationHelper().getInterpretation(exception);
+}
+
+class FailureInterpretation {
+  final String title;
+  final String message;
+
+  FailureInterpretation({required this.title, required this.message});
+}
+
+class FailuerInterpretationHelper {
+  final Map<Type, FailureInterpretation> _interpretations = {
+    UnauthorizedException: FailureInterpretation(
+      title: 'Unauthorized',
+      message: 'Please login to continue.',
+    ),
+    NetworkException: FailureInterpretation(
+      title: 'Connection Error',
+      message: 'Please check your internet connection and try again.',
+    ),
+    ForbiddenException: FailureInterpretation(
+      title: 'Forbidden',
+      message: 'You are not allowed to access this resource.',
+    ),
+    NotFoundException: FailureInterpretation(
+      title: 'Not Found',
+      message: 'The requested resource was not found.',
+    ),
+    BadRequestException: FailureInterpretation(
+      title: 'Error',
+      message:
+          'Unable to process your request, please check your inputs and try again.',
+    ),
+    InternalServerErrorException: FailureInterpretation(
+      title: 'Error',
+      message:
+          'Sorry, something went wrong at our end, please try again later.',
+    ),
+    ServiceUnavailableException: FailureInterpretation(
+      title: 'Error',
+      message:
+          'Sorry, our service is currently unavailable, please try again later.',
+    ),
+  };
+
+  FailureInterpretation getInterpretation(Exception exception) {
+    switch (exception) {
+      case UnauthorizedException():
+        return _interpretations[UnauthorizedException]!;
+      case NetworkException():
+        return _interpretations[NetworkException]!;
+      case ForbiddenException():
+        return _interpretations[ForbiddenException]!;
+      case NotFoundException():
+        return _interpretations[NotFoundException]!;
+      case BadRequestException():
+        return _interpretations[BadRequestException]!;
+      case InternalServerErrorException():
+        return _interpretations[InternalServerErrorException]!;
+      case ServiceUnavailableException():
+        return _interpretations[ServiceUnavailableException]!;
+      default:
+        return FailureInterpretation(
+            title: "Error", message: "Sorry, something went wrong.");
+    }
+  }
+}
